1b44e139f2454a30b472b05ec61d879f4e4f2e56
james-tierney <jamesytier@hotmail.com>
2024-03-10 20:02:42 +0000

    localhost changed to server and client domains

--- client/livingMemoire/src/components/Home.jsx
+++ client/livingMemoire/src/components/Home.jsx
@@ -8,8 +8,8 @@ function Home() {
 
   const generateQrCode = async () => {
     try {
-      const response = await axios.post('http://localhost:3002/generateQrCode', {
-        qrData: 'http://localhost:3002/signUp', // Replace with your desired QR data
+      const response = await axios.post('https://lovinglegacy.onrender.com/generateQrCode', {
+        qrData: 'https://lovinglegacy.onrender.com/signUp', // Replace with your desired QR data
       });
       setQrCodeData(response.data);
       console.log("qr code data = ", qrCodeData.data)
--- client/livingMemoire/src/components/temp/Profile.jsx
+++ client/livingMemoire/src/components/temp/Profile.jsx
@@ -51,7 +51,7 @@ export default function Profile() {
     // make a GET request to the server-side function
     const fetchData = async () => {
       try {
-        const response = await axios.get(`http://localhost:3002/userProfile`, {
+        const response = await axios.get(`https://lovinglegacy.onrender.com/userProfile`, {
           params : {
             username: username
           }
@@ -86,7 +86,7 @@ export default function Profile() {
         </header>
  
         <h2 className="text-lg font-semibold">{medallionProfile.firstName} {medallionProfile.lastName}</h2>
-        <img style={{height: '300px', width: 'auto'}}src='http://localhost:3002/medallionImages/testMarch-1709578295810-profile-pic.png'></img>
+        <img style={{height: '300px', width: 'auto'}}src='https://lovinglegacy.onrender.com/medallionImages/testMarch-1709578295810-profile-pic.png'></img>
         <p className="text-sm text-gray-600 mb-2">{medallionProfile.birthDate} - {medallionProfile.deathDate}</p>
         <div className="text-sm text-gray-600 mb-4">
           <p>City: {medallionProfile.city}</p>
