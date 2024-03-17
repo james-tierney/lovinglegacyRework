d182f23d938c4f2e4436301738b0cd94f02b998e
james-tierney <jamesytier@hotmail.com>
2024-03-17 11:50:29 +0000

    trying to fix content on medallion profilePage hiding under nav bar

--- client/livingMemoire/src/components/temp/Profile.jsx
+++ client/livingMemoire/src/components/temp/Profile.jsx
@@ -95,52 +95,52 @@ export default function Profile() {
   }
   
   return (
-    <div className="bg-gray-100 p-4">
+    <div className="bg-gray-100">
       <SiteNavigation  setIsNavigatedByApp={setIsNavigatedByApp} />
       <div className="max-w-sm mx-auto my-8 bg-white p-4 rounded-lg shadow-md">
-      <div className="flex items-center justify-between">
-       <img src={LogoSVG} alt="loving-legacy-logo" className='logo-img'/>
-      </div>
-      </div>
-      <div className='image-container'>
-        
-        <img style={{ maxWidth: '100%', height: 'auto'}} src={medallionProfile.profilePicture}></img>
-      </div>
-      
-      <div className="text-center">
-        {/* <h1>Profile Username for medallion {profileData.username}</h1> */}
-
- 
-        <h2 className="text-lg font-semibold">{medallionProfile.firstName} {medallionProfile.lastName}</h2>
-        <div className='flex items-center mb-6 underline-div'></div>
-        <p className="text-sm text-gray-600 mb-2">{medallionProfile.birthDate} - {medallionProfile.deathDate}</p>
-        <div className="text-sm text-gray-600 mb-4">
-          <p>City: {medallionProfile.city}</p>
-          <p>State: {medallionProfile.state}</p>
-          <p>Forest Lawn Memorial Park</p>
-          <p>Hollywood Hills, California</p>
-          <p>Plot Number: Lincoln Terrace section, Map #H89, Lot 5245, Companion Garden Crypt 2</p>
-          <div className='map-container'>
-            {showMap && <Map lat={medallionProfile.coordinates[0]} lng={medallionProfile.coordinates[1]} />}
-          </div>
-          <button className="text-blue-500 underline map-btn" onClick={toggleMap}>{mapText}</button>
+        <div className="flex items-center justify-between">
+          <img src={LogoSVG} alt="loving-legacy-logo" className='logo-img'/>
         </div>
-        <button className="share-btn bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 transition-colors mb-4">
-          Click to share!
-        </button>
-        <p>{medallionProfile.bio}</p>
-        <footer className="flex justify-around text-sm text-gray-600 border-t pt-2">
-          <a href="#" className={`footer-links ${activeLink === 'bio' ? 'active' : ''}`} onClick={(event) => handleLinkClick('bio', event)}>Bio</a>
-          <a href="#" className={`footer-links ${activeLink === 'photos' ? 'active' : ''}`} onClick={(event) => handleLinkClick('photos', event)}>Photos</a>
-          <a href="#" className={`footer-links ${activeLink === 'videos' ? 'active' : ''}`} onClick={(event) => handleLinkClick('videos', event)}>Videos</a>
-          <a href="#" className={`footer-links ${activeLink === 'links' ? 'active' : ''}`} onClick={(event) => handleLinkClick('links', event)}>Links</a>
+      </div>
+      <div className="container mx-auto p-4">
+        <div className='image-container'>
+          <img style={{ maxWidth: '100%', height: 'auto'}} src={medallionProfile.profilePicture}></img>
+        </div>
+        
+        <div className="text-center">
+          {/* <h1>Profile Username for medallion {profileData.username}</h1> */}
+   
+          <h2 className="text-lg font-semibold">{medallionProfile.firstName} {medallionProfile.lastName}</h2>
+          <div className='flex items-center mb-6 underline-div'></div>
+          <p className="text-sm text-gray-600 mb-2">{medallionProfile.birthDate} - {medallionProfile.deathDate}</p>
+          <div className="text-sm text-gray-600 mb-4">
+            <p>City: {medallionProfile.city}</p>
+            <p>State: {medallionProfile.state}</p>
+            <p>Forest Lawn Memorial Park</p>
+            <p>Hollywood Hills, California</p>
+            <p>Plot Number: Lincoln Terrace section, Map #H89, Lot 5245, Companion Garden Crypt 2</p>
+            <div className='map-container'>
+              {showMap && <Map lat={medallionProfile.coordinates[0]} lng={medallionProfile.coordinates[1]} />}
+            </div>
+            <button className="text-blue-500 underline map-btn" onClick={toggleMap}>{mapText}</button>
+          </div>
+          <button className="share-btn bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 transition-colors mb-4">
+            Click to share!
+          </button>
+          <p>{medallionProfile.bio}</p>
+          <footer className="flex justify-around text-sm text-gray-600 border-t pt-2">
+            <a href="#" className={`footer-links ${activeLink === 'bio' ? 'active' : ''}`} onClick={(event) => handleLinkClick('bio', event)}>Bio</a>
+            <a href="#" className={`footer-links ${activeLink === 'photos' ? 'active' : ''}`} onClick={(event) => handleLinkClick('photos', event)}>Photos</a>
+            <a href="#" className={`footer-links ${activeLink === 'videos' ? 'active' : ''}`} onClick={(event) => handleLinkClick('videos', event)}>Videos</a>
+            <a href="#" className={`footer-links ${activeLink === 'links' ? 'active' : ''}`} onClick={(event) => handleLinkClick('links', event)}>Links</a>
 
-          {/* Render appropriate component based on the active link */}
-          {activeLink === 'bio' && <ProfileBio bioText={medallionProfile.bio} />}
-          {activeLink === 'photos' && <ProfilePhotos />}
-          {activeLink === 'videos' && <ProfileVideos />}
-          {activeLink === 'links' && <ProfileLinks profileLinks={medallionProfile.linkToObituary} />}
-        </footer>
+            {/* Render appropriate component based on the active link */}
+            {activeLink === 'bio' && <ProfileBio bioText={medallionProfile.bio} />}
+            {activeLink === 'photos' && <ProfilePhotos />}
+            {activeLink === 'videos' && <ProfileVideos />}
+            {activeLink === 'links' && <ProfileLinks profileLinks={medallionProfile.linkToObituary} />}
+          </footer>
+        </div>
       </div>
     </div>
   );
